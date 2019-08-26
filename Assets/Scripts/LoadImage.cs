0ef9664dacd5506785f8620b8d4284295a6de361
kelly <kk.penaranda@uniandes.edu.co>
2019-08-25 21:10:18 -0500

    Se cargan imagenes y se muestran escenas iniciales

---
+++ Assets/Scripts/LoadImage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SFB
+{
+    public struct ExtensionFilter
+    {
+        public string Name;
+        public string[] Extensions;
+
+        public ExtensionFilter(string filterName, params string[] filterExtensions)
+        {
+            Name = filterName;
+            Extensions = filterExtensions;
+        }
+    }
+
+    public class LoadImage
+    {
+        private static ILoadImage _platformWrapper = null;
+
+        static LoadImage()
+        {
+#if UNITY_STANDALONE_OSX
+            _platformWrapper = new LoadImageMac();
+#elif UNITY_STANDALONE_WIN
+            _platformWrapper = new LoadImageWindows();
+#elif UNITY_STANDALONE_LINUX
+            _platformWrapper = new LoadImageLinux();
+#elif UNITY_EDITOR
+            _platformWrapper = new LoadImageEditor();
+#endif
+        }
+
+        /// <summary>
+        /// Native open file dialog
+        /// </summary>
+        /// <param name="title">Dialog title</param>
+        /// <param name="directory">Root directory</param>
+        /// <param name="extension">Allowed extension</param>
+        /// <param name="multiselect">Allow multiple file selection</param>
+        /// <returns>Returns array of chosen paths. Zero length array when cancelled</returns>
+        public static string[] OpenFilePanel(string title, string directory, string extension, bool multiselect)
+        {
+            var extensions = string.IsNullOrEmpty(extension) ? null : new[] { new ExtensionFilter("", extension) };
+            return OpenFilePanel(title, directory, extensions, multiselect);
+        }
+
+        /// <summary>
+        /// Native open file dialog
+        /// </summary>
+        /// <param name="title">Dialog title</param>
+        /// <param name="directory">Root directory</param>
+        /// <param name="extensions">List of extension filters. Filter Example: new ExtensionFilter("Image Files", "jpg", "png")</param>
+        /// <param name="multiselect">Allow multiple file selection</param>
+        /// <returns>Returns array of chosen paths. Zero length array when cancelled</returns>
+        public static string[] OpenFilePanel(string title, string directory, ExtensionFilter[] extensions, bool multiselect)
+        {
+            return _platformWrapper.OpenFilePanel(title, directory, extensions, multiselect);
+        }
+
+    }
+}
