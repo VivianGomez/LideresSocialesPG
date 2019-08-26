44bf97ffaed28b71e721a6c2bcb6eafae50a2a57
kelly <kk.penaranda@uniandes.edu.co>
2019-08-26 02:13:22 -0500

    Pequeño cambio en script de carga

--- Assets/Scripts/CargaImagen.cs
+++ Assets/Scripts/CargaImagen.cs
@@ -1,7 +1,5 @@
-﻿using System.Text;
+﻿
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -12,22 +10,6 @@ public class CargaImagen : MonoBehaviour, IPointerDownHandler
 {
     public RawImage output;
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-    //
-    // WebGL
-    //
-    [DllImport("__Internal")]
-    private static extern void UploadFile(string gameObjectName, string methodName, string filter, bool multiple);
-
-    public void OnPointerDown(PointerEventData eventData) {
-        UploadFile(gameObject.name, "OnFileUpload", ".png, .jpg", false);
-    }
-
-    // Called from browser
-    public void OnFileUpload(string url) {
-        StartCoroutine(OutputRoutine(url));
-    }
-#else
     //
     // Standalone platforms & editor
     //
@@ -45,7 +27,6 @@ public class CargaImagen : MonoBehaviour, IPointerDownHandler
             StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
         }
     }
-#endif
 
     private IEnumerator OutputRoutine(string url)
     {
