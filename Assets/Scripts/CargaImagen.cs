11f3c8a0c232b3895fc26e76568640b3505d6c7c
kelly <kk.penaranda@uniandes.edu.co>
2019-08-29 15:09:34 -0500

    Se cargan las imagenes de escenas y personaje a resources

--- Assets/Scripts/CargaImagen.cs
+++ Assets/Scripts/CargaImagen.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using SFB;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEditor;
 
 [RequireComponent(typeof(Button))]
 public class CargaImagen : MonoBehaviour, IPointerDownHandler
@@ -33,5 +36,20 @@ public class CargaImagen : MonoBehaviour, IPointerDownHandler
         var loader = new WWW(url);
         yield return loader;
         output.texture = loader.texture;
+
+        string file = Path.GetFileNameWithoutExtension(url);
+
+        Texture2D tex = new Texture2D(2, 2);
+        byte[] bytes;
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+        bytes = www.downloadHandler.data;
+        tex.LoadImage(bytes);
+        tex.EncodeToPNG();
+
+        string path = url.Replace("file:///", "").Replace("%20", " ");
+
+        FileUtil.CopyFileOrDirectory(path, "assets/Resources/" + file + ".png");
+        AssetDatabase.Refresh();
     }
 }
