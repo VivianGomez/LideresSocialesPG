991576115f24f79fbd3a9c3e5c9693df22651ea9
VivianGomez <vn.gomez@uniandes.edu.co>
2019-09-10 12:02:29 -0500

    Se agregan botones al canvas y barra de energía funcional que disminuye con el tiempo

---
+++ Assets/EnergyBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnergyBar : MonoBehaviour
+{
+	public Scrollbar energyBar;
+	public float Energy = 100;
+
+    void Update()
+    {
+        Hungry(0.1e-2f);
+    }
+
+    public void Hungry(float value)
+	{
+		Energy -= value;
+		energyBar.size = Energy / 100f;
+	}
+
+}
+
