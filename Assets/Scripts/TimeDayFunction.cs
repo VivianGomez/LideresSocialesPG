90c2d7eba4a98e810cfc4feea564d51e4b501601
kelly <kk.penaranda@uniandes.edu.co>
2019-09-11 02:05:29 -0500

    Cambio automatico del dia y reinicio de los datos

--- Assets/Scripts/TimeDayFunction.cs
+++ Assets/Scripts/TimeDayFunction.cs
@@ -7,18 +7,20 @@ using UnityEngine.UI;
 
 public class TimeDayFunction : MonoBehaviour
 {
-    private float inicio;
-    private int hora;
-    private double ultimoSegundo;
+    public float inicio;
+    public int hora;
+    public int dia;
+    public double ultimoSegundo;
     public TextMeshPro texto;
     public SpriteRenderer fondoEscena;
-    float red;
-    float green;
-    float blue;
+    public float red;
+    public float green;
+    public float blue;
 
     // Start is called before the first frame update
     void Start()
     {
+        dia = 1;
         red = 0.8f;
         green = 0.8f;
         blue = 0.8f;
@@ -30,47 +32,54 @@ public class TimeDayFunction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(hora!= 24)
-        {
-            float t = Time.time - inicio;
-            string minutos = ((int)t / 60).ToString();
-            string segundos = (t % 60).ToString("f1");
-            //print(minutos + ":"+segundos);
-
-            double segundoActual = double.Parse(segundos);
-
-            if (segundoActual != 0.0 && segundoActual % 12.50 == 0)
+        GameObject player = GameObject.Find("Personaje");
+        
+            if (hora != 24)
             {
+                float t = Time.time - inicio;
+                string minutos = ((int)t / 60).ToString();
+                string segundos = (t % 60).ToString("f1");
+                //print(minutos + ":"+segundos);            
 
-                double valorAbsoluto = Math.Abs(ultimoSegundo - segundoActual);
 
-                if (valorAbsoluto != 0)
+                double segundoActual = double.Parse(segundos);
+
+                if (segundoActual != 0.0 && segundoActual % 12.50 == 0)
                 {
-                    hora = hora + 1;
-                    print(segundoActual);
-                    texto.text="Día 1 - Hora: " + hora + ":00";
-                    print("cambia la hora, hora " + hora + ":00");
-                    if (hora > 17)
+
+                    double valorAbsoluto = Math.Abs(ultimoSegundo - segundoActual);
+
+                    if (valorAbsoluto != 0)
                     {
-                        Color color = new Color(red, green, blue, 1)
+                        hora = hora + 1;
+                        //print(segundoActual);
+                        //texto.text="Día " + dia + " - Hora: " + hora + ":00";
+                        //print("cambia la hora, hora " + hora + ":00");
+                        if (hora > 17)
                         {
-                            r = red,
-                            g = green,
-                            b = blue,
-                            a = 1
-                        };
+                            Color color = new Color(red, green, blue, 1)
+                            {
+                                r = red,
+                                g = green,
+                                b = blue,
+                                a = 1
+                            };
 
-                        fondoEscena.color = color;
+                            fondoEscena.color = color;
 
-                        red = red - 0.1f;
-                        green = green - 0.1f;
-                        blue = blue - 0.1f;
+                            red = red - 0.1f;
+                            green = green - 0.1f;
+                            blue = blue - 0.1f;
+                        }
                     }
-                }
 
-                ultimoSegundo = segundoActual;
+                    ultimoSegundo = segundoActual;
+                }
+                texto.text = "Día " + dia + " - Hora: " + hora + ":00";
             }
-        }
+          
+
+        
         
     }
 }
