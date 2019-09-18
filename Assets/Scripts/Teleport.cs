1d9f8b82d144d5d01257deb1df0add4ec197d3ed
kelly <kk.penaranda@uniandes.edu.co>
2019-09-18 05:04:28 -0500

    Creacion de escenas de la casa y recorrido entre ellas

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public string levelToLoad;
+    //public LoadSceneTransition ls;
+
+
+    public void OnTriggerEnter2D(Collider2D cTrigger)
+    {
+
+        if (cTrigger.tag != "Player") { return; }
+
+        SceneManager.LoadSceneAsync(levelToLoad);
+        //ls.LoadImageCanvas();
+    }
+}
