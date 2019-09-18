a85219aceba2ddf604acf12ac554aa5c404d489e
Vivian <vn.gomez@uniandes.edu.co>
2019-09-18 14:05:18 -0500

    sonido ambiente y primera versión de animaciones al iniciar el juego

---
+++ Assets/Scripts/PanelInicioFin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PanelInicioFin : MonoBehaviour
+{
+    public GameObject panelIF;
+    public Image imagenLider;
+    public TextMeshProUGUI  infoLider;
+
+    void Start()
+    {
+        infoLider.text = "";
+        StartCoroutine(quitImage());
+    }
+
+     IEnumerator quitImage()
+    { 
+        yield return new WaitForSeconds(3);
+        imagenLider.sprite = null;
+        infoLider.text = "Kevin Julian León";
+        yield return new WaitForSeconds(4);
+        imagenLider.enabled = false;
+        panelIF.SetActive(false);
+    }
+}
