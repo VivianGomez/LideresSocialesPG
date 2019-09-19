1e98e053c8499ff4a2b492b5d6b79af6694be790
kelly <kk.penaranda@uniandes.edu.co>
2019-09-19 00:42:37 -0500

    Camina e intento de dormir

--- Assets/Scripts/DayChanger.cs
+++ Assets/Scripts/DayChanger.cs
@@ -24,6 +24,9 @@ public class DayChanger : MonoBehaviour
 
     public void onFadeComplete()
     {
+        GameObject p = GameObject.Find("Personaje");
+        p.GetComponent<Movimiento>().animator.SetTrigger("despierta");
+
         GameObject fondo = GameObject.Find("Background");
         GameObject camara = GameObject.Find("Main Camera");
         
@@ -39,13 +42,13 @@ public class DayChanger : MonoBehaviour
         print("se aumento el dia");
 
         camara.GetComponent<JSONWriter>().reescribirJSON(fondo.GetComponent<TimeDayFunction>().dia, new int[] { int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento1.text), int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento2.text), int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento3.text), int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento4.text), int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento5.text), int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento6.text), int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento7.text), int.Parse(camara.GetComponent<JSONLoaderJuego0>().cantidadAlimento8.text) }, (int)camara.GetComponent<EnergyBar>().Energy, fondo.GetComponent<TimeDayFunction>().hora, false, fondo.GetComponent<TimeDayFunction>().inicio, fondo.GetComponent<TimeDayFunction>().ultimoSegundo, fondo.GetComponent<TimeDayFunction>().segundoActual, new string[] { "Arroz", "Huevos", "Agua", "Arroz con leche", "Chocolate", "Dulces", "Granos", "Pan" });
+        
     }
 
     public void pasarEstado()
     {
         GameObject p = GameObject.Find("Personaje");
-        p.GetComponent<Movimiento>().animator.SetTrigger("despierta");
-
+        
         p.GetComponent<Movimiento>().trigger = false;
                 
     }
