00a5d0a7370dd7f65a21cfe029c9f1dd7be2ea5d
Vivian <vn.gomez@uniandes.edu.co>
2019-09-24 18:20:42 -0500

    Arreglos bug barra de energia y adición de cursores

---
+++ Assets/Scripts/ClickableObjectCursor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObjectCursor : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        if(gameObject.name.Equals("Nevera"))
+        {
+            CursorController.Instance.SetNeveraCursor();
+        }
+        else if (gameObject.name.Equals("mom"))
+        {
+            CursorController.Instance.SetMomCursor();
+        }
+        else if(gameObject.name.Equals("camaClick"))
+        {
+            CursorController.Instance.SetDormirCursor();
+        }
+        else if(gameObject.name.Equals("noticias") || gameObject.name.Equals("ButtonNews"))
+        {
+            CursorController.Instance.SetInfoCursor();
+        }
+        else if(gameObject.name.Equals("close"))
+        {
+            CursorController.Instance.SetCloseCursor();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        CursorController.Instance.SetMoveCursor();
+    }
+}
