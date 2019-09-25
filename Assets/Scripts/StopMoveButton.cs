70325dcf27bca2095872e51c4fa7bd789ad7b618
Vivian <vn.gomez@uniandes.edu.co>
2019-09-25 01:53:07 -0500

    Animaciones botones canvas

---
+++ Assets/Scripts/StopMoveButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopMoveButton : MonoBehaviour
+{
+    public Animator animationBtn;
+    
+    void Start()
+    {
+        animationBtn.enabled = true;
+    }
+
+    public void clickBtn()
+    {
+        animationBtn.enabled = false;
+    }
+}
