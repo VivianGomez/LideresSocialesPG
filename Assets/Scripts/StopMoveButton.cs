bf567d2c985401e4e37266be3f6349c0f6769060
Vivian <vn.gomez@uniandes.edu.co>
2019-09-25 15:39:55 -0500

    Se modifica el script de animaciones del canvas, de modo que el regalo sólo se mueva si no ha sido abierto

--- Assets/Scripts/StopMoveButton.cs
+++ Assets/Scripts/StopMoveButton.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections;
+﻿using LitJson;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class StopMoveButton : MonoBehaviour
 {
     public Animator animationBtn;
-    
+    public JsonData jsonData;
+
+    private bool tomo;
+
     void Start()
-    {
-        animationBtn.enabled = true;
+    {        
+        if ( this.name.Equals("ButtonGift") && File.Exists(Application.dataPath + "/Gamedata.json"))
+        {
+            jsonData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Gamedata.json"));
+            tomo = ("0").Equals(jsonData[4]);
+            animationBtn.enabled = tomo;
+        }
+        else{
+            animationBtn.enabled = true;
+        }
     }
 
     public void clickBtn()
