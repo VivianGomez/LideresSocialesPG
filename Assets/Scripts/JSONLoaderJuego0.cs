2b6376e67f2ddc7328c444c21485ec23bbd8e29f
kelly <kk.penaranda@uniandes.edu.co>
2019-11-26 17:30:14 -0500

    El loading espera a que se cargue todo

--- Assets/Scripts/JSONLoaderJuego0.cs
+++ Assets/Scripts/JSONLoaderJuego0.cs
@@ -194,7 +194,7 @@ public class JSONLoaderJuego0 : MonoBehaviour
 
            gameData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Gamedata.json"));
            LoadInfoAlimentos(gameData[10]);
-            StartCoroutine(loading());
+           
         }
         else
         {
@@ -250,7 +250,8 @@ public class JSONLoaderJuego0 : MonoBehaviour
         {                
             backgroundSprite.sprite = Sprite.Create(wwwBG.texture, new Rect(0, 0, wwwBG.texture.width, wwwBG.texture.height), new Vector2(0.5f, 0.5f));
             Vector2 S = backgroundSprite.sprite.bounds.size;
-            
+            StartCoroutine(loading());
+
         }
         else
         {
@@ -388,116 +389,7 @@ public class JSONLoaderJuego0 : MonoBehaviour
         p.GetComponent<Movimiento>().DialogoNPC.SetActive(false);
         AbrirRegaloLiderazgo();
     }
-
-    public IEnumerator createSaveAnim(string nombre, bool loop)
-    {
-
-        Sprite[] sprites = Resources.LoadAll<Sprite>(nombre);// load all sprites in "assets/Resources/nombre" folder
-        print(sprites.Length);
-        print(nombre);
-        AnimationClip animClip = new AnimationClip();
-        animClip.frameRate = 25;   // FPS
-        EditorCurveBinding spriteBinding = new EditorCurveBinding();
-        spriteBinding.type = typeof(SpriteRenderer);
-        spriteBinding.path = "";
-        spriteBinding.propertyName = "m_" + nombre;
-        ObjectReferenceKeyframe[] spriteKeyFrames = new ObjectReferenceKeyframe[sprites.Length];
-        for (int i = 0; i < (sprites.Length); i++)
-        {
-            spriteKeyFrames[i] = new ObjectReferenceKeyframe();
-            spriteKeyFrames[i].time = i;
-            spriteKeyFrames[i].value = sprites[i];
-        }
-
-        AnimationClipSettings animClipSett = new AnimationClipSettings();
-        animClipSett.loopTime = loop;
-
-        AnimationUtility.SetAnimationClipSettings(animClip, animClipSett);
-
-        AnimationUtility.SetObjectReferenceCurve(animClip, spriteBinding, spriteKeyFrames);
-
-        AssetDatabase.CreateAsset(animClip, "assets/Resources/"+"nombre.anim");
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-
-        yield return sprites;
-    }
-
-    private IEnumerator OutputRoutine(WWW url, int width, int height, bool loop)
-    {
-        string file = Path.GetFileNameWithoutExtension(url.url);
-        //print("archivo se llamaria:::" + file);
-
-        Texture2D tex = new Texture2D(2, 2);
-        byte[] bytes;
-        UnityWebRequest www = UnityWebRequest.Get(url.url);
-        yield return www.SendWebRequest();
-        bytes = www.downloadHandler.data;
-        tex.LoadImage(bytes);
-        tex.EncodeToPNG();
-
-        WebClient client = new WebClient();
-        if (!File.Exists("assets/Resources/" + file + ".png"))
-        {
-            client.DownloadFile(url.url, @"assets/Resources/" + file + ".png");
-            AssetDatabase.Refresh();
-
-            StartCoroutine(ProcesarTextura("assets/Resources/" + file + ".png", width, height));
-            StartCoroutine(createSaveAnim(file, loop));
-
-
-        }
-        loadingSpriteStart.SetActive(false);
-
-        yield return true;
-
-    }
-
-    IEnumerator ProcesarTextura(string path, int SliceWidth, int SliceHeight)
-    {
-        Texture2D texture = Resources.Load<Texture2D>(Path.GetFileNameWithoutExtension(path));
-        var importer = AssetImporter.GetAtPath(path) as TextureImporter;
-
-        importer.spriteImportMode = SpriteImportMode.Multiple;
-        importer.mipmapEnabled = true;
-        importer.filterMode = FilterMode.Point;
-        importer.spritePivot = Vector2.zero;
-        importer.textureCompression = TextureImporterCompression.Uncompressed;
-
-        var textureSettings = new TextureImporterSettings();
-
-        importer.ReadTextureSettings(textureSettings);
-        textureSettings.spriteMeshType = SpriteMeshType.Tight;
-        textureSettings.spriteExtrude = 0;
-
-        importer.SetTextureSettings(textureSettings);
-
-        List<SpriteMetaData> newData = new List<SpriteMetaData>();
-
-
-        for (int i = 0; i < texture.width; i += SliceWidth)
-        {
-            for (int j = 0; j < texture.height; j += SliceHeight)
-            {
-                SpriteMetaData smd = new SpriteMetaData();
-                smd.pivot = Vector2.zero;
-                smd.alignment = (int)SpriteAlignment.Center;
-                smd.name = (j) / SliceHeight + ", " + i / SliceWidth;
-                smd.rect = new Rect(i, j, SliceWidth, SliceHeight);
-
-                newData.Add(smd);
-            }
-        }
-        importer.spritesheet = newData.ToArray();
-
-
-        AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
-        AssetDatabase.Refresh();
-
-        yield return newData.ToArray();
-
-    }
-
+   
     void LoadInfoAlimentos(JsonData alimentos)
     {
         buttonAlimento1.text= "" + alimentos[0];
